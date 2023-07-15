fa2945f412b7d18e04018d128b134fbc913c0bd1
ShalevZorany <134508865+ShalevZorany@users.noreply.github.com>
2023-07-15 19:31:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Initialize game variables
 let step = 1;
 let score = 0;
-let totalScore = 0; // New variable to track the cumulative score
+let totalScore = 0;
 let secretNumberDigits = 3;
 let guessDigits = 0;
 let secretNumber = generateSecretNumber(secretNumberDigits);
@@ -110,7 +110,7 @@ function processGuess(guess) {
       guessTableBody.innerHTML = '';
     } else {
       // Game Over
-      alert('Congratulations! You have completed all the steps. Final Score: ' + totalScore);
+      alert('Congratulations! You have completed all the steps.\nFinal Score: ' + totalScore + '\nGame Over');
       // You can add any additional logic or actions for the end of the game here
     }
   }
